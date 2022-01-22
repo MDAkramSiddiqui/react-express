587526e95a513c6e63da0b38ce03f9c7dd74d0a2
Akram1 <mohammad.akram1@SW-LP12447.local>
2022-01-23 02:36:00 +0530

    minor fixes

--- client/webpack/dev.config.js
+++ client/webpack/dev.config.js
@@ -4,10 +4,8 @@ const commonConfig = require('./common.config');
 module.exports = merge(commonConfig, {
     devServer: {
 		historyApiFallback: true,
-		port: 3000,
 		proxy: {
 			"/api": "http://localhost:9090",
 		},
 	},
-	mode: "development",
 })
